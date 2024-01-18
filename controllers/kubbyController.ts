207d222daaaf0f81e71b04ea0b3a0f3603d3e2b0
eddy8mame <132296192+eddy8mame@users.noreply.github.com>
2024-01-18 15:39:39 -0500

    Added additional PromClient gauges, returning data using KubernetesClientAPI

--- controllers/kubbyController.ts
+++ controllers/kubbyController.ts
@@ -4,7 +4,7 @@ import client from 'prom-client'
 const KubeNamespace = 'kube-system';
 const kc = new k8s.KubeConfig();
 
-kc.loadFromFile('./edwins.yaml');
+kc.loadFromFile('./edwinscluster-config.yaml');
 
 const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
@@ -14,29 +14,92 @@ interface KubbyController {
 }
 
 
-const countNodes = new client.Gauge({
-  name: 'numNodes',
-  help: 'This is number of nodes',
+const totalNodes = new client.Gauge({
+  name: 'totalNodes',
+  help: 'This is total number of nodes',
 });
 
+const totalCores = new client.Gauge({
+    name: 'totalCores',
+    help: 'This is total number of CPU cores',
+});
+
+const totalAllocatableCores = new client.Gauge({
+    name: 'totalCPUCores',
+    help: 'This is total number of allocatable CPU cores',
+});
+
+const totalMemory = new client.Gauge({
+    name: 'totalMemory',
+    help: 'This is total number of memory',
+});
+
+const totalAllocatableMemory = new client.Gauge({
+    name: 'totalAllocatableMemory',
+    help: 'This is total number of allocatable memory',
+});
 
 const kubbyController: KubbyController = {
   getClusterInfo: async () => {
     const nodeResponse = await k8sApi.listNode()
-    const numNodes = nodeResponse.body.items
-    countNodes.set(numNodes.length)
-    // console.log(numNodes.length)
-    const metrics = await client.register.metrics()
-    console.log(metrics.toString())
-    // console.log(counter.get())
-    
+    const nodes = nodeResponse.body.items
+    totalNodes.set(nodes.length)
+
+        let cpuCores = 0;
+        let allocatableCores = 0;
+        let memoryBytes = 0; 
+        let allocatableMemoryBytes = 0;
+        nodes.forEach((node) => { 
+
+            if (node.status?.capacity) {
+                cpuCores += parseInt(node.status.capacity.cpu);
+                memoryBytes += parseKubernetesMemoryString(node.status.capacity.memory);
+            }
+
+            if (node.status?.allocatable) {
+                allocatableCores += parseCpuStringToCores(node.status.allocatable.cpu);
+                allocatableMemoryBytes += parseKubernetesMemoryString(node.status.allocatable.memory);
+            }
+            
+        });
+
+        totalNodes.set(nodes.length);
+        totalCores.set(cpuCores);
+        totalAllocatableCores.set(allocatableCores);
+        const totalMemoryRD = +convertKiBToGiB(memoryBytes).toFixed(2);
+        totalMemory.set(totalMemoryRD);
+        const totalAllocatableMemoryRD = +convertKiBToGiB(allocatableMemoryBytes).toFixed(2);
+        totalAllocatableMemory.set(totalAllocatableMemoryRD);
+
+        const metrics = await client.register.metrics()
+        console.log(metrics)
+
   },
   getMetrics: async() => {
 
   }
 }
 
+function convertKiBToGiB(kibytes: number) {
+    return kibytes / Math.pow(2,30 ); // 1048576 is 2^20
+}
 
+function parseCpuStringToCores(cpuString: string) { 
+    if (cpuString.endsWith('m')) { 
+        return parseInt(cpuString.slice(0, -1), 10) / 1000;
+    }
+    return parseInt(cpuString, 10);
+}
 
+function parseKubernetesMemoryString(memString: string) {
+    const units: { [key: string]: number } = {
+        Ki: 1024,
+        Mi: 1024 * 1024,
+        Gi: 1024 * 1024 * 1024,
+      };
+    const regex = /(\d+)([KMG]i)/;
+    const [, num, unit] = memString.match(regex) || [];
+    return num ? parseInt(num, 10) * (units[unit] || 1) : 0;
+  }
 
 export default kubbyController;
