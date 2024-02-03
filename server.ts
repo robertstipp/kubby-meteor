e07df3de372b79810c2edc1337df9592cb548e3e
Nick Doolan <nickdoolan31@gmail.com>
2024-02-03 13:56:58 -0500

    FixedWS/ND

--- server.ts
+++ server.ts
@@ -21,7 +21,13 @@ async function run() {
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    // origin: ["http://localhost:3001", "http://localhost:8080"],
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
